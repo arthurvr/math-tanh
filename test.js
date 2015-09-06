c4cf9573a6091b4b484c32d34d945911ff70ca35
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 21:11:39 +0200

    use AVA for testing

--- test.js
+++ test.js
@@ -1,46 +1,52 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 var requireUncached = require('require-uncached');
 var floatEqual = require('float-equal');
 
 Math.tanh = undefined;
 var mathTanh = require('./');
 
-describe('tanh', function () {
-	it('Should use the native method when possible', function () {
-		Math.tanh = function () {
-			return 'foo';
-		};
+test('Should use the native method when possible', function (t) {
+	Math.tanh = function () {
+		return 'foo';
+	};
 
-		assert.equal(requireUncached('./')(), 'foo');
-	});
+	t.is(requireUncached('./')(), 'foo');
+	t.end();
+});
 
-	it('Should handle NaN by returning NaN', function () {
-		assert.ok(isNaN(mathTanh(NaN)));
-	});
+test('Should handle NaN by returning NaN', function (t) {
+	t.true(isNaN(mathTanh(NaN)));
+	t.end();
+});
 
-	it('Should return +0 when passing +0', function () {
-		assert.deepEqual(mathTanh(+0), 0);
-	});
+test('Should return +0 when passing +0', function (t) {
+	t.is(mathTanh(+0), 0);
+	t.end();
+});
 
-	it('Should return -0 when passing -0', function () {
-		assert.deepEqual(mathTanh(-0), 0);
-	});
+test('Should return -0 when passing -0', function (t) {
+	t.is(mathTanh(-0), 0);
+	t.end();
+});
 
-	it('Should return 1 when passing infinity', function () {
-		assert.deepEqual(mathTanh(Infinity), 1);
-	});
+test('Should return 1 when passing infinity', function (t) {
+	t.is(mathTanh(Infinity), 1);
+	t.end();
+});
 
-	it('Should return -1 when passing -infinity', function () {
-		assert.deepEqual(mathTanh(-Infinity), -1);
-	});
+test('Should return -1 when passing -infinity', function (t) {
+	t.is(mathTanh(-Infinity), -1);
+	t.end();
+});
 
-	it('Should return the hyperbolic tangent', function () {
-		assert(floatEqual(mathTanh(1), 0.7615941559557649));
-		assert(floatEqual(mathTanh(-1), -0.7615941559557649));
-		assert(floatEqual(mathTanh(0.2), 0.19737532022490412));
-		assert(floatEqual(mathTanh(-0.2), -0.19737532022490412));
-		assert.equal(mathTanh(30), 1);
-		assert.equal(mathTanh(-50), -1);
-	});
+test('Should return the hyperbolic tangent', function (t) {
+	t.true(floatEqual(mathTanh(1), 0.7615941559557649));
+	t.true(floatEqual(mathTanh(-1), -0.7615941559557649));
+	t.true(floatEqual(mathTanh(0.2), 0.19737532022490412));
+	t.true(floatEqual(mathTanh(-0.2), -0.19737532022490412));
+	t.is(mathTanh(30), 1);
+	t.is(mathTanh(-50), -1);
+
+	t.end();
 });
