015000a45ace8ff90c336172afead7f2bf85a6a4
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-04 10:45:28 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-'use strict';
-var test = require('ava');
-var requireUncached = require('require-uncached');
-var floatEqual = require('float-equal');
+import test from 'ava';
+import requireUncached from 'require-uncached';
+import floatEqual from 'float-equal';
 
 Math.tanh = undefined;
-var mathTanh = require('./');
+import mathTanh from './';
 
-test('Should use the native method when possible', function (t) {
+test('Should use the native method when possible', t => {
 	Math.tanh = function () {
 		return 'foo';
 	};
@@ -15,32 +14,32 @@ test('Should use the native method when possible', function (t) {
 	t.end();
 });
 
-test('Should handle NaN by returning NaN', function (t) {
+test('Should handle NaN by returning NaN', t => {
 	t.true(isNaN(mathTanh(NaN)));
 	t.end();
 });
 
-test('Should return +0 when passing +0', function (t) {
+test('Should return +0 when passing +0', t => {
 	t.is(mathTanh(+0), 0);
 	t.end();
 });
 
-test('Should return -0 when passing -0', function (t) {
+test('Should return -0 when passing -0', t => {
 	t.is(mathTanh(-0), 0);
 	t.end();
 });
 
-test('Should return 1 when passing infinity', function (t) {
+test('Should return 1 when passing infinity', t => {
 	t.is(mathTanh(Infinity), 1);
 	t.end();
 });
 
-test('Should return -1 when passing -infinity', function (t) {
+test('Should return -1 when passing -infinity', t => {
 	t.is(mathTanh(-Infinity), -1);
 	t.end();
 });
 
-test('Should return the hyperbolic tangent', function (t) {
+test('Should return the hyperbolic tangent', t => {
 	t.true(floatEqual(mathTanh(1), 0.7615941559557649));
 	t.true(floatEqual(mathTanh(-1), -0.7615941559557649));
 	t.true(floatEqual(mathTanh(0.2), 0.19737532022490412));
